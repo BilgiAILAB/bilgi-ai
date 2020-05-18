3b5f466d192b9a8963a555b455d0583590701896
Enes Gül <Enes.gul@bilgiedu.net>
2020-05-18 23:58:53 +0300

    [fix] hdp model errors

--- topic_modelling/distributions.py
+++ topic_modelling/distributions.py
@@ -8,6 +8,13 @@ def word_distribution(model, n_topic):
     return word_distributions
 
 
+def hdp_word_distribution(model, topics_n):
+    word_distributions = []
+    for topic_id in topics_n:
+        word_distributions.append([list(word) for word in model.show_topic(topic_id, topn=20)])
+    return word_distributions
+
+
 def lda_topic_distribution(doc_number, model, corpus):
     topic_distributions = []
     for i in range(doc_number):
@@ -40,3 +47,17 @@ def lsi_doc_distribution(n_topic, doc_number, model, corpus):
     for i in range(doc_number):
         doc_dist[topic_distance.get_topic_dist_max(model[corpus[i]])[0]].append(i)
     return doc_dist
+
+
+def hdp_doc_distribution(n_topic, topics_n, doc_number, model, corpus):
+    doc_dist = {}
+    for i in range(n_topic):
+        doc_dist.update({i: []})
+
+    for i in range(doc_number):
+        doc_dist[topic_distance.get_topic_dist_max(model[corpus[i]])[0]].append(i)
+
+    last_dist = {}
+    for element in topics_n:
+        last_dist.update({topics_n.index(element): doc_dist.get(element)})
+    return last_dist
--- topic_modelling/hdp_web.py
+++ topic_modelling/hdp_web.py
@@ -3,22 +3,30 @@ from topic_modelling import preprocess, coherence, distributions, topic_distance
 
 
 def HDP(corpus, n_topic):
+    n_topic = 150
     cleaned_data, data_tokens, id2word, corpus = preprocess.preprocess(corpus=corpus)
     doc_number = len(data_tokens)
     hdp_model = HdpModel(corpus=corpus, id2word=id2word)
     coherence_v = coherence.coherence_value(model=hdp_model, tokens=data_tokens, dictionary=id2word)
-    word_distributions = distributions.word_distribution(model=hdp_model, n_topic=n_topic)
+
     topic_distributions = distributions.lsi_topic_distribution(doc_number=doc_number, model=hdp_model, corpus=corpus)
-
-    topic_number = len(hdp_model.show_topics(150))
-
-    doc_dist = distributions.lsi_doc_distribution(n_topic=topic_number, doc_number=doc_number, model=hdp_model, corpus=corpus)
+    topics_n = []
+    for document in topic_distributions:
+        for topic_dist in document:
+            if topic_dist[0] not in topics_n:
+                topics_n.append(topic_dist[0])
+            first_index = topic_distributions.index(document)
+            second_index = topic_distributions[first_index].index(topic_dist)
+            topic_distributions[first_index][second_index][0] = topics_n.index(topic_dist[0])
+    word_distributions = distributions.hdp_word_distribution(model=hdp_model, topics_n=topics_n)
+    doc_dist = distributions.hdp_doc_distribution(n_topic=n_topic,topics_n=topics_n, doc_number=doc_number, model=hdp_model, corpus=corpus)
 
     output = {"filecount": doc_number,
               "coherence_value": float(coherence_v),
               "word_distributions": word_distributions,
               "topic_distributions": topic_distributions,
               "doc_dist": doc_dist,
+              "topics_n":topics_n,
               }
 
     return output
