ab0a85f4f1e31d4cdfde1ba964430c7253a32dd2
Enes Gül <Enes.gul@bilgiedu.net>
2020-05-25 15:24:31 +0300

    [feat] 3d graph is added

--- topic_modelling/algorithms/topic_graph.py
+++ topic_modelling/algorithms/topic_graph.py
@@ -5,9 +5,12 @@ from bokeh.models import HoverTool, ColumnDataSource
 from bokeh.resources import CDN
 from bokeh.embed import file_html
 import numpy as np
+import plotly.express as px
+import pandas as pd
+import plotly
 
 
-def tsne_graph(output, topic_names, doc_names, algorithm):
+def tsne_graph_3d(output, topic_names, doc_names, algorithm):
     n_topics = len(topic_names)
     topic_distributions = output.get('topic_distributions')
     data_tokens = output.get('data_tokens')
@@ -15,7 +18,94 @@ def tsne_graph(output, topic_names, doc_names, algorithm):
     if algorithm.lower()=="hdp":
         topic_distributions = fill_with_zero(n_topics, topic_distributions)
 
+    colorArray = [
+        "#63b598", "#ce7d78", "#ea9e70", "#0d5ac1",
+        "#14a9ad", "#4ca2f9",
+        "#d298e2", "#6119d0", "#d2737d", "#c0a43c", "#f2510e",
+        "#651be6", "#79806e", "#cd2f00", "#9348af",
+        "#c5a4fb", "#996635", "#b11573", "#4bb473", "#75d89e",
+        "#2f3f94", "#2f7b99", "#da967d", "#34891f", "#ca4751",
+        "#4b5bdc", "#250662", "#cb5bea", "#228916",
+        "#ac3e1b", "#df514a", "#539397", "#880977", "#f697c1",
+        "#f158bf", "#e145ba", "#ee91e3", "#4834d0",
+        "#802234", "#6749e8", "#8fb413", "#b2b4f0",
+        "#c3c89d", "#c9a941", "#41d158", "#fb21a3", "#51aed9",
+        "#5bb32d", "#0807fb", "#21538e", "#89d534", "#d36647",
+        "#7fb411", "#0023b8", "#3b8c2a", "#986b53", "#f50422",
+        "#983f7a", "#ea24a3", "#79352c", "#521250", "#c79ed2",
+        "#63b598", "#ce7d78", "#ea9e70", "#0d5ac1",
+        "#14a9ad", "#4ca2f9",
+        "#d298e2", "#6119d0", "#d2737d", "#c0a43c", "#f2510e",
+        "#651be6", "#79806e", "#cd2f00", "#9348af",
+        "#c5a4fb", "#996635", "#b11573", "#4bb473", "#75d89e",
+        "#2f3f94", "#2f7b99", "#da967d", "#34891f", "#ca4751",
+        "#4b5bdc", "#250662", "#cb5bea", "#228916",
+        "#ac3e1b", "#df514a", "#539397", "#880977", "#f697c1",
+        "#f158bf", "#e145ba", "#ee91e3", "#4834d0",
+        "#802234", "#6749e8", "#8fb413", "#b2b4f0",
+        "#c3c89d", "#c9a941", "#41d158", "#fb21a3", "#51aed9",
+        "#5bb32d", "#0807fb", "#21538e", "#89d534", "#d36647",
+        "#7fb411", "#0023b8", "#3b8c2a", "#986b53", "#f50422",
+        "#983f7a", "#ea24a3", "#79352c", "#521250", "#c79ed2",
+        "#63b598", "#ce7d78", "#ea9e70", "#0d5ac1",
+        "#14a9ad", "#4ca2f9",
+        "#d298e2", "#6119d0", "#d2737d", "#c0a43c", "#f2510e",
+        "#651be6", "#79806e", "#cd2f00", "#9348af",
+        "#c5a4fb", "#996635", "#b11573", "#4bb473", "#75d89e",
+        "#2f3f94", "#2f7b99", "#da967d", "#34891f", "#ca4751",
+        "#4b5bdc", "#250662", "#cb5bea", "#228916",
+        "#ac3e1b", "#df514a", "#539397", "#880977", "#f697c1",
+        "#f158bf", "#e145ba", "#ee91e3", "#4834d0",
+        "#802234", "#6749e8", "#8fb413", "#b2b4f0",
+        "#c3c89d", "#c9a941", "#41d158", "#fb21a3", "#51aed9",
+        "#5bb32d", "#0807fb", "#21538e", "#89d534", "#d36647",
+        "#7fb411", "#0023b8", "#3b8c2a", "#986b53", "#f50422",
+        "#983f7a", "#ea24a3", "#79352c", "#521250", "#c79ed2"]
+    freq_per_doc = []
+    for document in data_tokens:
+        most_vocab = Counter()
+        for word in document:
+            most_vocab[word] += 1
+        freq_per_doc.append([word_tuple[0] for word_tuple in most_vocab.most_common(3)])
 
+    topic_weights = []
+    for document in topic_distributions:
+        topic_weights.append([abs(float(topic[1])) for topic in document])
+    colorArrayy = np.array(colorArray)
+    topic_num = np.argmax(topic_weights, axis=1)
+    tsne_model = TSNE(n_components=3, verbose=1, random_state=0, angle=.99, init='pca')
+    tsne_lda = tsne_model.fit_transform(topic_weights)
+
+    df = pd.DataFrame()
+    df['x'] = tsne_lda[:, 0]
+    df['y'] = tsne_lda[:, 1]
+    df['z'] = tsne_lda[:, 2]
+    df['topic_name'] = np.array(topic_names)[topic_num]
+    df['file_name'] = doc_names
+    df['frequent_words'] = freq_per_doc
+    fig = px.scatter_3d(df,
+                        x='x',
+                        y='y',
+                        z='z',
+                        color='topic_name',
+                        opacity=0.8,
+                        size_max=18,
+                        hover_data=['topic_name', 'file_name', 'frequent_words'],
+                        )
+
+    html_plot = plotly.offline.plot(fig, filename='plotly3d.html', auto_open=False)
+    with open('plotly3d.html', 'r') as f:
+        html = f.read()
+    return html
+
+
+def tsne_graph_2d(output, topic_names, doc_names, algorithm):
+    n_topics = len(topic_names)
+    topic_distributions = output.get('topic_distributions')
+    data_tokens = output.get('data_tokens')
+
+    if algorithm.lower()=="hdp":
+        topic_distributions = fill_with_zero(n_topics, topic_distributions)
 
     colorArray = [
         "#63b598", "#ce7d78", "#ea9e70", "#0d5ac1",
@@ -75,6 +165,7 @@ def tsne_graph(output, topic_names, doc_names, algorithm):
     tsne_model = TSNE(n_components=2, verbose=1, random_state=0, angle=.99, init='pca')
     tsne_lda = tsne_model.fit_transform(topic_weights)
 
+
     labelss = np.array(topic_names)
     source = ColumnDataSource(dict(
         x=tsne_lda[:, 0],
@@ -107,21 +198,12 @@ def tsne_graph(output, topic_names, doc_names, algorithm):
                  alpha=0.9,
                  size=10
                  )
-    '''
-    items_legend = []
-    order=0
-    for name in topic_names:
-        items_legend.append(LegendItem(label=name, renderers=[r], index=order))
-        order = order+1
 
-    legend = Legend(items=items_legend)
-    plot.add_layout(legend)
-    '''
-    # hover tools
     hover = plot.select(dict(type=HoverTool))
     # hover.tooltips = {"content": "Title: @title"}
     plot.legend.location = "top_left"
     return file_html(plot, CDN, "tsne_lda_graph")
+
 
 
 def fill_with_zero(n_topics, topic_distributions):
